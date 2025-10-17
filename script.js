526476900cecfbba8686ca176c6fc1ee1f4e70e9
ShoppingVIPs <ramondanielcrespo@gmail.com>
2025-10-17 09:02:59 -0400

    Shopping

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Abrir imagen ampliada
+
 function abrirImagen(img) {
   const modal = document.getElementById("imagenModal");
   const imagenAmpliada = document.getElementById("imagenAmpliada");
@@ -6,12 +6,11 @@ function abrirImagen(img) {
   modal.style.display = "flex";
 }
 
-// Cerrar modal
+
 function cerrarImagen() {
   document.getElementById("imagenModal").style.display = "none";
 }
 
-// Cerrar con click fuera de la imagen
 window.onclick = function(event) {
   const modal = document.getElementById("imagenModal");
   if (event.target === modal) modal.style.display = "none";
